95f197126a5e67f81d229d0ccd113a5134ec3dbb
t4p3w0rm <jeremi@humanity.org>
2024-12-13 00:40:54 -0500

    add docs for BMToken contract

--- contracts/BMToken.sol
+++ contracts/BMToken.sol
@@ -3,75 +3,33 @@ pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "./interfaces/IReverseToken.sol";
 
+/// @title A simple Ownable and Upgradeable token.
 contract BMToken is Initializable, OwnableUpgradeable, ERC20Upgradeable {
+
+    /// @notice This contract is meant to be initialized and shouldn't do anything in its constructor.
     constructor() {
         _disableInitializers();
     }
 
+    /// @notice Initializes the token's state. This can only be called once in the lifetime of the token (after initial deployment of the first version).
+    /// @param _initialOwner the address to use as the initial owner of this token.
     function initialize(address _initialOwner) public initializer {
         __Ownable_init(_initialOwner);
         __ERC20_init("BMToken", "BMTN");
     }
 
+    /// @notice Mint tokens to an address. Only the owner can call this.
+    /// @param _account the address which will receive the tokens.
+    /// @param _value the amount of tokens to mint to the account.
     function mint(address _account, uint256 _value) public onlyOwner {
         _mint(_account, _value);
     }
 
+    /// @notice Burn tokens from an address. Only the owner can call this.
+    /// @param _account the address which will see some of its tokens burned.
+    /// @param _value the amount of tokens to burn from the address.
     function burn(address _account, uint256 _value) public onlyOwner {
         _burn(_account, _value);
     }
-
-    // /// @notice Allow the Arb One bridge to mint tokens
-    // function bridgeMint(address account, uint256 amount)
-    //     public
-    //     override(IReverseToken)
-    //     // onlyArbOneGateway
-    // {
-    //     _mint(account, amount);
-    // }
-
-    // /// @notice Allow the Arb One bridge to burn tokens
-    // function bridgeBurn(address account, uint256 amount)
-    //     public
-    //     override(IReverseToken)
-    //     // onlyArbOneGateway
-    // {
-    //     _burn(account, amount);
-    // }
-
-    /// @dev shouldRegisterGateway is set to true when in `registerTokenOnL2`
-    // function isBMEnabled() external view override returns (uint8) {
-    //     require(shouldRegisterGateway, "L1BMToken: not expecting gateway registration");
-    //     return uint8(MAGIC_BM_ONE);
-    // }
-
-    /// @notice Register the token on both Arb One and Nova
-    /// @dev    Called once by anyone immediately after the contract is deployed
-    // function registerTokenOnL2(RegistrationParams memory novaParams) public payable {
-    //     // we temporarily set `shouldRegisterGateway` to true for the callback in registerTokenToL2 to succeed
-    //     // this is so that we can be sure that this contract does currently mean to be
-    //     // doing a registration
-    //     bool prev = shouldRegisterGateway;
-    //     shouldRegisterGateway = true;
-
-    //     IL1CustomGateway(bmGateway).registerTokenToL2{value: novaParams.valueForGateway}(
-    //         novaParams.l2TokenAddress,
-    //         novaParams.maxGasForCustomGateway,
-    //         novaParams.gasPriceBid,
-    //         novaParams.maxSubmissionCostForCustomGateway,
-    //         novaParams.creditBackAddress
-    //     );
-
-    //     IGatewayRouter(bmRouter).setGateway{value: novaParams.valueForRouter}(
-    //         novaGateway,
-    //         novaParams.maxGasForRouter,
-    //         novaParams.gasPriceBid,
-    //         novaParams.maxSubmissionCostForRouter,
-    //         novaParams.creditBackAddress
-    //     );
-
-    //     shouldRegisterGateway = prev;
-    // }
 }
