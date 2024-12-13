3162f803d8c9e7a88350790016abc65553a88e84
t4p3w0rm <jeremi@humanity.org>
2024-12-13 00:43:08 -0500

    Add title to BMTokenUpgrade test contract

--- contracts/test/BMTokenUpgrade.sol
+++ contracts/test/BMTokenUpgrade.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 import { BMToken } from "../BMToken.sol";
 
+/// @title Test contract to verify if adding functions to the base token works. It does. You can ignore this.
 contract BMTokenUpgrade is BMToken {
     event TestEvent(
         string data
